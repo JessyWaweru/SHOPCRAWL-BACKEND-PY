59c686b766f2a6528ba38a1fdbe93bf8fea63972
Jessy <bryanjessywaweru@gmail.com>
2026-02-06 06:06:53 -0800

    :rocket:reverted connection max age to 0

--- shopcrawl_backend/settings.py
+++ shopcrawl_backend/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'shopcrawl_backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=0
     )
 }
 
