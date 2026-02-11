edb9e8ee1c2868e62a7da14c3264cd771c2a159d
Jessy <bryanjessywaweru@gmail.com>
2026-02-11 07:02:08 -0800

    :test_tube:testing a change to manage.py to force IPV4 instant connection

--- manage.py
+++ manage.py
@@ -2,7 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import socket
 
+# --- START FORCE IPv4 PATCH ---
+# This forces the app to ignore IPv6 addresses to prevent the 30s timeout on Render
+try:
+    original_getaddrinfo = socket.getaddrinfo
+    def new_getaddrinfo(*args, **kwargs):
+        res = original_getaddrinfo(*args, **kwargs)
+        # Filter out any IPv6 results (AF_INET6)
+        return [r for r in res if r[0] == socket.AF_INET]
+    socket.getaddrinfo = new_getaddrinfo
+except Exception:
+    pass
+# --- END FORCE IPv4 PATCH ---
 
 def main():
     """Run administrative tasks."""
@@ -16,7 +29,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
