d23e5dbd49291e08083cbf4b7fe9e6f037ef86e8
Jessy <bryanjessywaweru@gmail.com>
2026-02-11 06:16:21 -0800

    :test_tube:added a ping test

--- shopcrawl_backend/urls.py
+++ shopcrawl_backend/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.http import JsonResponse # <--- Import this
+
+# 1. Simple Test Function (No Database)
+def ping(request):
+    return JsonResponse({"message": "Pong! Server is fast."})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # This connects your new routes!
+    path('api/', include('api.urls')),
+    path('ping/', ping),  # <--- Add this line
 ]
